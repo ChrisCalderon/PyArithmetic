d94db6534eb7cd15024d72605ab86a40553efa8f
Christian Calderon <calderon.christian760@gmail.com>
2013-12-18 18:20:03 -0800

    made point_table a little cleaner
    added a Table class to make handling the pretty table generation
    simpler/prettier in the point_table function.

--- arithmetic.py
+++ arithmetic.py
@@ -27,26 +27,33 @@ class Expression(object):
 	def substitute(self, x):
 		return self.format_string.format(x)
 
-def point_table(expression, lims):
+RIGHT = ">"
+LEFT = "<"
+CENTER = "^"
+
+class Table(object):
+	def __init__(self, data, uniform=False, columnwise=True, align=">", min_width=3):
+		"""Data is an object that iterates over the rows of the Table"""
+		self.data = data
+		if columnwise: #each column has a uniform width
+			lens = [max([len(str(datum)) for datum in column]+[min_width]) for column in zip(*data)]
+			self.format_string = "|"+"|".join("{"+":"+align+"{}".format(n)+"}" for n in lens)+"|"
+
+	def __str__(self):
+		rows = [self.format_string.format(*row) for row in self.data]
+		sep = "-"*len(rows[0])
+		return "\n".join([sep]+sum([[row, sep] for row in rows], []))
+
+def point_table(expression, *lims):
 	expression = Expression(expression)
 	xs = range(*lims)
 	ys = map(expression, xs)
 	expressions = map(expression.substitute, xs)
 	points = zip(xs, ys)
-	lengths = [max(map(len, map(str, list_))+[3]) for list_ in [xs, expressions, ys, points]]
-	format_string = '|'+'|'.join(" {"+":>{0}".format(length)+"} " for length in lengths)+'|'
-	labels = format_string.format('x', expression, 'y', '(x, y)')
-	sep = '-'*len(labels)
+	xs, expressions, ys, points = ["x"]+xs, [str(expression)]+expressions, ["y"]+ys, ["(x, y)"]+points
+	table = Table(zip(xs, expressions, ys, points))
+	print str(table)
 
-	print sep
-	print labels
-	print sep
-	for i, x in enumerate(xs):
-		print format_string.format(x, expressions[i], ys[i], points[i])
-		print sep
-
-def long_division(dividend, divisor):
-	pass
 
 if __name__=="__main__":
-	point_table("x^2 + 1", [-2,3])
+	point_table("x^2 + 1", -2,3)
